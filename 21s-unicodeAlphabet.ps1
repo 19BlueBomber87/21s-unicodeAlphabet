5820e7ab69ed058c375d36d8a11efeafc59cd783
Mark Kruse87 <a19mkruse87k@outlook.com>
2021-06-03 15:17:48 -0800

    Update output .txt files to correct output Uncomment and  $showOuput part of the .ps1 code

--- 21s-unicodeAlphabet.ps1
+++ 21s-unicodeAlphabet.ps1
@@ -1,14 +1,14 @@
 New-Item -ItemType file unicodeAlphabet-PS.txt
 New-Item -ItemType file unicodeAlphabet-PS-Wrapped.txt
 function create_unicodeAlphabet {
-    foreach($i in 0..65994){
+    foreach($i in 0..65000){
         $uniPoint = [char]$i
         Write-Output $uniPoint | Out-File -Append -NoNewline unicodeAlphabet-PS.txt
     }
 
 }
 $count = 0
-function create_unicodeAlphabet {
+function create_unicodeAlphabetWrap {
     foreach($i in 0..65000){
         $uniPoint = [char]$i
         $count ++
@@ -22,12 +22,12 @@ function create_unicodeAlphabet {
 
 }
 create_unicodeAlphabet
-# $showOutput = Read-Host View unicode Alphabet?
-# if ($showOutput -eq "yes"){
-#     notepad.exe unicodeAlphabet-PS.txt
-# }
-# else{
-#     Write-Verbose -Message "<@:D" -Verbose *>&1
-# }
-# New-Item -ItemType file -path abc123.txt
-# Write-Output yahoo`nyahoo | Out-File -Append -NoNewline abc123.txt
+create_unicodeAlphabetWrap
+$showOutput = Read-Host View unicode Alphabet?
+if ($showOutput -eq "yes"){
+    notepad.exe unicodeAlphabet-PS.txt
+    notepad.exe unicodeAlphabet-PS-Wrapped.txt
+}
+else{
+    Write-Verbose -Message "<@:D" -Verbose *>&1
+}
